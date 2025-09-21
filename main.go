b4df9f107735f9badd18894c604558deb940491f
eselse <ilikewebdevelopment@gmail.com>
2025-09-21 22:47:30 +0400

    add new variables and count values for tham

--- main.go
+++ main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	usd := 45
+	const fromUSDtoEUR = 0.85
+	const fromUSDtoRUB = 83.49
+	rstEUR := float64(usd) * fromUSDtoEUR
+	rstRUB := float64(usd) * fromUSDtoRUB
+	fmt.Println(rstEUR, rstRUB)
 }
