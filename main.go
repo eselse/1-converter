93dfbc4ca86bd1f57dc26e9a04b2b2db186b98c1
eselse <ilikewebdevelopment@gmail.com>
2025-09-21 22:51:14 +0400

    add new variables and count values for them

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import "fmt"
 func main() {
 	usd := 45
 	const fromUSDtoEUR = 0.85
-	const fromUSDtoRUB = 83.49
+	const fromEURtoRUB = 83.49
 	rstEUR := float64(usd) * fromUSDtoEUR
-	rstRUB := float64(usd) * fromUSDtoRUB
-	fmt.Println(rstEUR, rstRUB)
+	rstRUB := rstEUR * fromEURtoRUB
+	fmt.Println(rstRUB)
 }
